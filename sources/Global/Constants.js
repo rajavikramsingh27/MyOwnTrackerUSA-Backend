7141ac0abbfd716ccd41b6c8d53dc7b3ec2741d3
rajavikramsingh27 <rajavikramsingh27@gmail.com>
2022-06-24 10:23:02 +0530

    mongo connected

--- sources/Global/Constants.js
+++ sources/Global/Constants.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     PORT: process.env.PORT || 3001,
-    MONGOURI: 'mongodb+srv://Wen:Wen@wen.klyri25.mongodb.net/?retryWrites=true&w=majority',
+    MONGOURI: 'mongodb+srv://MyOwnTrackerUSA:MyOwnTrackerUSA@myowntrackerusa.stjzs0f.mongodb.net/?retryWrites=true&w=majority',
     JWT_SECRET: 'JSONWEBSECRET',
     kBaseURL: 'localhost:3001/',
     kBaseURLLive: 'wenbackend.herokuapp.com/',
