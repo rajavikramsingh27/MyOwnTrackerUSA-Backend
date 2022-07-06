b8efa60bea0d4b6a8621d7d8e7d61c10cb9ad949
rajavikramsingh27 <rajavikramsingh27@gmail.com>
2022-07-06 20:49:09 +0530

    adf

--- sources/Routers/Invoice.js
+++ sources/Routers/Invoice.js
@@ -7,12 +7,12 @@ const router = express.Router()
 const Invoice = require('../Controllers/Invoice')
 const AuthController = require('../Controllers/Auth')
 
-router.post('/createInvoice', AuthController.auth, Invoice.createEstimate)
-router.get('/readInvoice', AuthController.auth, Invoice.readEstimate)
-router.post('/updateInvoice', AuthController.auth, Invoice.updateEstimate)
-router.post('/updateInvoiceStates', AuthController.auth, Invoice.updateEstimateStates)
-router.post('/updateInvoiceStatesName', AuthController.auth, Invoice.updateEstimateStatesName)
-router.post('/deleteInvoice', AuthController.auth, Invoice.deleteEstimate)
+router.post('/createInvoice', AuthController.auth, Invoice.createInvoice)
+router.get('/readInvoice', AuthController.auth, Invoice.readInvoice)
+router.post('/updateInvoice', AuthController.auth, Invoice.updateInvoice)
+router.post('/updateStates', AuthController.auth, Invoice.updateInvoiceStates)
+router.post('/updateInvoiceStatesName', AuthController.auth, Invoice.updateInvoiceStatesName)
+router.post('/deleteInvoice', AuthController.auth, Invoice.deleteInvoice)
 
 module.exports = router
 
