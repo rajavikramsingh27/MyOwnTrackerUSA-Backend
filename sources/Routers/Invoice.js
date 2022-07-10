6bcb42158a4d866403fc8aa29d9b12d5e6b2f233
rajavikramsingh27 <rajavikramsingh27@gmail.com>
2022-07-10 23:53:50 +0530

    asfd

--- sources/Routers/Invoice.js
+++ sources/Routers/Invoice.js
@@ -12,6 +12,7 @@ router.get('/readInvoice', AuthController.auth, Invoice.readInvoice)
 router.post('/updateInvoice', AuthController.auth, Invoice.updateInvoice)
 router.post('/updateInvoiceStates', AuthController.auth, Invoice.updateInvoiceStates)
 router.post('/updateInvoiceStatesName', AuthController.auth, Invoice.updateInvoiceStatesName)
+router.post('/updateInvoiceSignature', AuthController.auth, Invoice.updateInvoiceSignature)
 router.post('/deleteInvoice', AuthController.auth, Invoice.deleteInvoice)
 
 module.exports = router
